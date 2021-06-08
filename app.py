b1f75d56b6042775e29db11bb177eb11bf40be26
monic <monicaleonor98@gmail.com>
2021-06-08 14:56:01 -0600

    heroku website

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from logic.person_logic import PersonLogic
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    logic = PersonLogic()
+    personList = logic.getAllPerson()
+    return render_template("index.html", personList=personList)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
